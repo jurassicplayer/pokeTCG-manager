599c65ed03d79be3984ffc5c709ea095c74929b5
jurassicplayer <jurassicplayer75@gmail.com>
2017-11-29 15:18:35 -0800

    Added some more scraping.

--- main.py
+++ main.py
@@ -18,6 +18,11 @@ expansion='ex11'
 #expansion='dp4'
 #card_num='73'
 
+#Ability Alolan Raichu
+series='sm-series'
+expansion='sm4'
+card_num='31'
+
 database = sqlite3.connect("ptcg.db")
 c = database.cursor()
 with urllib.request.urlopen('{}/{}/{}/{}'.format(poke_url, series, expansion, card_num)) as response:
@@ -37,16 +42,23 @@ with urllib.request.urlopen('{}/{}/{}/{}'.format(poke_url, series, expansion, ca
         for poketype in poketypes:
             card_poketype.append(poketype.find('i').get('class')[1][5:])
         ##card_ability = soup.find_all(class_="ability").find('div').find('h1').text##
+        #card_poke_ability = [ soup.find(class_='poke-ability').text,  soup.find(class_='pokemon-abilities').find('h3').text.split()[len(soup.find(class_='poke-ability').text):], soup.find(class_='pokemon-abilities').find('p').text ]
+        #print(soup.find(class_='pokemon-abilities').find('h3').text) #.split())
         card_stats = soup.find_all(class_="stat")
         for stat in card_stats:
             stat_category = stat.find('h4').text
             if stat_category == 'Weakness':
                 try:
                     weakness_type = stat.find('ul').find('li').get('title')
-                    weakness_modifier = stat.find('ul').find('li').text.split()
+                    weakness_modifier = stat.find('ul').find('li').text.split()[0]
                 except:
                     print('No weaknesses?')
             if stat_category == 'Resistance':
+                try:
+                    resistance_type = stat.find('ul').find('li').get('title')
+                    resistance_modifier = stat.find('ul').find('li').text.split()[0]
+                except:
+                    print('No resistances?')
                 pass
             if stat_category == 'Retreat Cost':
                 try:
@@ -56,4 +68,5 @@ with urllib.request.urlopen('{}/{}/{}/{}'.format(poke_url, series, expansion, ca
     card_expansion = soup.find(class_='stats-footer').find('h3').find('a').text
     card_number = soup.find(class_='stats-footer').find('span').text.split()[0].split('/')
     card_expansion_icon = soup.find(class_="expansion").find('a').find('i').get('style')
-    print(card_preevolution)
+    card_illustrator = soup.find(class_="illustrator").find('h4').find('a').text
+    print(card_illustrator)
