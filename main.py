7a76049af137bf69125ea01f4c6b701568a52e59
jurassicplayer <jurassicplayer75@gmail.com>
2017-12-03 15:15:28 -0800

    Changed to using PokemonTCG.io for data scraping (convenience).

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 import urllib.request
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 import sqlite3
+import json
+from pokemontcgsdk import Card
+from pokemontcgsdk import Set
+from pokemontcgsdk import Type
+from pokemontcgsdk import Supertype
+from pokemontcgsdk import Subtype
 
 series='sm-series'
 expansion='sm4'
 card_num='1'
-poke_url='https://www.pokemon.com/us/pokemon-tcg/pokemon-cards'
+#poke_url='https://www.pokemon.com/us/pokemon-tcg/pokemon-cards'
 
 #Dual color Azumarill
-series='ex-series'
-expansion='ex11'
+#series='ex-series'
+#expansion='ex11'
 #card_num='19'
 
 #Weakness modifier +20 dmg Kakuna
@@ -19,10 +25,38 @@ expansion='ex11'
 #card_num='73'
 
 #Ability Alolan Raichu
-series='sm-series'
-expansion='sm4'
-card_num='31'
+#series='sm-series'
+#expansion='sm4'
+#card_num='31'
 
+cards = Card.where(set='generations').where(supertype='pokemon').all()
+for card in cards:
+    card_name = card.name
+    card_id = card.id
+    card_national_pokedex_number = card.national_pokedex_number
+    card_image_SD = card.image_url
+    card_image_HD = card.image_url_hi_res
+    card_types = card.types
+    card_supertype = card.supertype
+    card_subtype = card.subtype
+    card_hp = card.hp
+    card_number = card.number
+    card_illustrator = card.artist
+    card_rarity = card.rarity
+    card_series = card.series
+    card_expansion = card.expansion
+    card_expansion_code = card.set_code
+    card_expansion_code_ptcgo = ""  #<FIXME>
+    card_text = card.text
+    card_ability = card.ability
+    card_ancient_trait = card.ancient_trait
+    card_attacks = card.attacks
+    card_weaknesses = card.weaknesses
+    card_resistances = card.resistances
+    card_retreat_cost = card.retreat_cost
+    print(card.__dict__)
+
+'''
 database = sqlite3.connect("ptcg.db")
 c = database.cursor()
 with urllib.request.urlopen('{}/{}/{}/{}'.format(poke_url, series, expansion, card_num)) as response:
@@ -41,6 +75,8 @@ with urllib.request.urlopen('{}/{}/{}/{}'.format(poke_url, series, expansion, ca
         poketypes = soup.find(class_="card-basic-info").find(class_="right").find_all('a')
         for poketype in poketypes:
             card_poketype.append(poketype.find('i').get('class')[1][5:])
+        card_pokemon_abilities = soup.find(class_="pokemon-abilities")
+        card_pokemon_attacks = card_pokemon_abilities.find_all(class_="ability")
         ##card_ability = soup.find_all(class_="ability").find('div').find('h1').text##
         #card_poke_ability = [ soup.find(class_='poke-ability').text,  soup.find(class_='pokemon-abilities').find('h3').text.split()[len(soup.find(class_='poke-ability').text):], soup.find(class_='pokemon-abilities').find('p').text ]
         #print(soup.find(class_='pokemon-abilities').find('h3').text) #.split())
@@ -69,4 +105,5 @@ with urllib.request.urlopen('{}/{}/{}/{}'.format(poke_url, series, expansion, ca
     card_number = soup.find(class_='stats-footer').find('span').text.split()[0].split('/')
     card_expansion_icon = soup.find(class_="expansion").find('a').find('i').get('style')
     card_illustrator = soup.find(class_="illustrator").find('h4').find('a').text
-    print(card_illustrator)
+    print(card_pokemon_attacks)
+'''
