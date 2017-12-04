e3f74c902ab4288e6a0ab58b53d72bbe7e30e214
jurassicplayer <jurassicplayer75@gmail.com>
2017-12-04 14:55:33 -0800

    Added some post processing of the energy data from PokemonTCG.io to add to table.

--- main.py
+++ main.py
@@ -43,14 +43,14 @@ def create_connection(db_file):
     return None
 
 def insert_card(card):
-    card_id = card.id
-    card_name = card.name
-    card_national_pokedex_number = card.national_pokedex_number
+    card_id = card.id                                   #Pokemon card id (Official Pokemon website)
+    card_name = card.name                               #Pokemon Name
+    card_national_dex = card.national_pokedex_number    #National Pokedex Number
     card_image_SD = card.image_url
     card_image_HD = card.image_url_hi_res
-    card_types = card.types
-    card_supertype = card.supertype
-    card_subtype = card.subtype
+    card_types = card.types                             #Pokemon Type
+    card_supertype = card.supertype                     #Pokemon, Trainer, Energy
+    card_subtype = card.subtype                         #Stage 1, Stage 2, Item, Supporter, Special Energy
     card_hp = card.hp
     card_number = card.number
     card_illustrator = card.artist
@@ -66,31 +66,93 @@ def insert_card(card):
     card_weaknesses = card.weaknesses
     card_resistances = card.resistances
     card_retreat_cost = card.retreat_cost
-    t = (card_id,
-         card_name,
-         card_expansion_code,
-         card_number,
-         card_national_pokedex_number,
-         card_types,
-         card_subtype,
-         card_supertype,
-         card_illustrator,
-         card_image_SD,
-         card_image_HD,
-         card_rarity,
-         card_hp,
-         card_text,
-         card_weaknesses,
-         card_resistances,
-         card_retreat_cost,
-         card_ancient_trait,
-         card_ability,
-         card_ability,
-         card_ability)
+    card_index = (
+        card_id,
+        card_name,
+        card_expansion_code,
+        card_number,
+        card_national_dex,
+        #card_types,
+        card_subtype,
+        card_supertype,
+        card_illustrator,
+        card_image_SD,
+        card_image_HD,
+        card_rarity,
+        card_hp,
+        card_text,
+        card_weaknesses,
+        card_resistances,
+        #card_retreat_cost,
+        card_ancient_trait,
+        card_ability,
+        card_ability,
+        card_ability
+        )
+    insert_energy_cost(card_id, 'Pokemon Type', card_types)
+    insert_energy_cost(card_id, 'Retreat', card_retreat_cost)
     
     c = conn.cursor()
-    print("INSERT INTO Card VALUES {}".format(t))
-    c.execute("INSERT INTO Card VALUES {}".format(t))
+    print("INSERT INTO Card VALUES {}".format(card_index))
+    # c.execute("INSERT INTO Card VALUES {}".format(card_index))
+
+def insert_energy_cost(card_id, energy_cost_type, energy_cost_array, energy_cost_attackID=None):
+    print(
+        "Inserting energy:\n{}\n{}\n{}\n{}".format(card_id, energy_cost_type, energy_cost_array, energy_cost_attackID)
+    )
+    energy_cost_converted = 0
+    colorless_energy = 0
+    darkness_energy = 0
+    fairy_energy = 0
+    fighting_energy = 0
+    fire_energy = 0
+    grass_energy = 0
+    lightning_energy = 0
+    metal_energy = 0
+    psychic_energy = 0
+    water_energy = 0
+    
+    if not energy_cost_array:
+    
+    for energy in energy_cost_array:
+        if 'Colorless' in energy:
+            colorless_energy    += 1
+        if 'Darkness' in energy:
+            darkness_energy     += 1
+        if 'Fairy' in energy:
+            fairy_energy        += 1
+        if 'Fighting' in energy:
+            fighting_energy     += 1
+        if 'Fire' in energy:
+            fire_energy         += 1
+        if 'Grass' in energy:
+            grass_energy        += 1
+        if 'Lightning' in energy:
+            lightning_energy    += 1
+        if 'Metal' in energy:
+            metal_energy        += 1
+        if 'Psychic' in energy:
+            psychic_energy      += 1
+        if 'Water' in energy:
+            water_energy        += 1
+        energy_cost_converted   += 1
+    
+    energy_index = (
+        card_id,
+        energy_cost_type,       ## Pokemon Type, Retreat, Attack
+        energy_cost_attackID,   ## None, None, Attack Name
+        energy_cost_converted,  ## Converted cost (number)
+        colorless_energy,
+        darkness_energy,
+        fairy_energy,
+        fighting_energy,
+        fire_energy,
+        grass_energy,
+        lightning_energy,
+        metal_energy,
+        psychic_energy,
+        water_energy
+        )
 
 cards = Card.where(set='generations').where(supertype='pokemon').all()
 conn = create_connection("ptcg.db")
